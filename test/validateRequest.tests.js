082fe64e7b9d5f3bf4451f0fcdc236aed3d575ae
Marc Peters <mr.marc.peters@gmail.com>
2024-08-30 15:34:18 -0700

    [MP] #86duh6atz Add a flag which fixes a bug where the request body could be validated in two different ways, only one of which matches the schema that was provided

--- test/validateRequest.tests.js
+++ test/validateRequest.tests.js
@@ -61,10 +61,6 @@ describe('validateRequest', () => {
     });
 
     describe('when the validation settings specify that empty strings are not treated the same as undefined values', () => {
-      beforeEach(() => {
-        validationSettings.treatEmptyStringsLikeUndefinedValues = false;
-      });
-
       it('should allow a required string parameter to be an empty string', () => {
         models.RequestBody.required = ['id'];
         req.body = {id: ''};
@@ -82,14 +78,54 @@ describe('validateRequest', () => {
     });
 
     describe('when the validation settings specify that objects are not allowed to have additional properties by default', () => {
-      beforeEach(() => {
-        validationSettings.objectsCanHaveAnyAdditionalPropertiesByDefault = false;
-      });
-
       it('should not allow an object to have additional properties which are not specified in the schema', () => {
         req.body = {id: '1', someExtraProperty: 'some value'}
         const result = validateRequest(requestSchema, req, models, {objectsCanHaveAnyAdditionalPropertiesByDefault: false});
         assertValidationFailed(result, ['object contains invalid properties: someExtraProperty']);
+      });
+    });
+  });
+
+  describe('validation of the request body', () => {
+    beforeEach(() => {
+      models.RequestBody.required = ['id'];
+    });
+
+    it('should allow the request body to have two contradictory schemas, one of which does not match the schema that is provided to the function (legacy behaviour)', () => {
+      requestSchema.parameters[0].name = 'requestBody';
+
+      req.body = {
+        id: '1'
+      };
+      let result = validateRequest(requestSchema, req, models);
+      assertValidationPassed(result);
+
+      req.body = {
+        requestBody: {
+          id: '1'
+        }
+      };
+      result = validateRequest(requestSchema, req, models);
+      assertValidationPassed(result);
+    });
+
+    describe('when the validation settings specify that the request body is not allowed to have two contradictory schemas', () => {
+      it('should not allow an object to have additional properties which are not specified in the schema', () => {
+        requestSchema.parameters[0].name = 'requestBody';
+
+        req.body = {
+          id: '1'
+        };
+        let result = validateRequest(requestSchema, req, models, {requestBodyCanHaveTwoContradictorySchemas: false});
+        assertValidationPassed(result);
+
+        req.body = {
+          requestBody: {
+            id: '1'
+          }
+        };
+        result = validateRequest(requestSchema, req, models, {requestBodyCanHaveTwoContradictorySchemas: false});
+        assertValidationFailed(result, ['id is required']);
       });
     });
   });
