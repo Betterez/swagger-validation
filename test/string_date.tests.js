c00702d479fe4833abe0221f857188586b19db99
Marc Peters <mr.marc.peters@gmail.com>
2024-09-20 15:52:03 -0700

    [MP] #86dunnz8n Remove support for string "date" format (controllable with a flag)

--- test/string_date.tests.js
+++ test/string_date.tests.js
@@ -1,3 +1,4 @@
+const {expect} = require('chai');
 const moment = require('moment');
 const helper = require('./test_helper');
 const {assertValidationPassed, assertValidationFailed} = helper;
@@ -64,5 +65,44 @@ describe('string - date', function () {
       validationSettings
     });
     assertValidationFailed(ret, ["testParam is not valid based on the pattern for moment.ISO 8601"]);
+  });
+
+  describe('when the validation settings specify that the "date" format is unsupported', () => {
+    beforeEach(() => {
+      validationSettings.allowStringsToHaveUnreliableDateFormat = false;
+    });
+
+    it('should not allow a string to have the "date" format', function () {
+      const result = validateParameter({
+        schema: {
+          type: 'string',
+          format: 'date'
+        },
+        value: '2014-01-01',
+        models,
+        validationContext,
+        validationSettings
+      });
+      assertValidationFailed(result, ['Swagger schema is invalid: string has "date" format, but this format is unreliable because it parses dates without considering timezones.  Use the "date-time" format instead, or define a "pattern" to validate the format of the date string, and parse it yourself.']);
+    });
+
+    describe('when the validation settings specify that an error should be thrown when there is a problem with the swagger schema', () => {
+      beforeEach(() => {
+        validationSettings.throwErrorsWhenSchemaIsInvalid = true;
+      });
+
+      it('should throw an error when a string has the "date" format', function () {
+        expect(() => validateParameter({
+          schema: {
+            type: 'string',
+            format: 'date'
+          },
+          value: '2014-01-01',
+          models,
+          validationContext,
+          validationSettings
+        })).to.throw('Swagger schema is invalid: string has "date" format, but this format is unreliable because it parses dates without considering timezones.  Use the "date-time" format instead, or define a "pattern" to validate the format of the date string, and parse it yourself.');
+      });
+    });
   });
 });
